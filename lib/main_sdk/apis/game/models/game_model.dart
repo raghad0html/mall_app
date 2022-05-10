123df4d63c54d6be286cd4fa63f37b4f865aceb4
abdalrhmnnassan <abdalrhmnnassan1000@gmail.com>
2022-05-10 15:44:57 +0300

    --add game id to game model

--- lib/main_sdk/apis/game/models/game_model.dart
+++ lib/main_sdk/apis/game/models/game_model.dart
@@ -1,18 +1,21 @@
 class GameModel {
   bool? gameAccepted;
   String? msg;
+  String? gameId;
 
   GameModel({this.gameAccepted, this.msg});
 
   GameModel.fromJson(Map<String, dynamic> json) {
     gameAccepted = json['gameAccepted'];
     msg = json['msg'];
+    gameId = json['game_id'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data =  <String, dynamic>{};
     data['gameAccepted'] = gameAccepted;
     data['msg'] = msg;
+    data['game_id'] = gameId;
     return data;
   }
 }
