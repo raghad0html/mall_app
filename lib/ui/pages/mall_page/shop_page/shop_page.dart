86268d71d681010cdd92e4f515713d0819e5bfdf
abdalrhmnnassan <abdalrhmnnassan1000@gmail.com>
2022-05-15 01:12:44 +0300

    --init shop page

---
+++ lib/ui/pages/mall_page/shop_page/shop_page.dart
@@ -0,0 +1,15 @@
+import  'package:flutter/material.dart';
+
+class ShopsPage extends StatefulWidget {
+  const ShopsPage({Key? key}) : super(key: key);
+
+  @override
+  State<ShopsPage> createState() => _ShopsPageState();
+}
+
+class _ShopsPageState extends State<ShopsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
