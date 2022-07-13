45f46d6452fdc00c155fc71f33f6dd90503adae1
WAMERK <66085818+WAMERK@users.noreply.github.com>
2022-07-13 16:31:55 +0200

    Update custom_dialog_contest_tips.dart

--- lib/ui/widget/custom_dialog_contest_tips.dart
+++ lib/ui/widget/custom_dialog_contest_tips.dart
@@ -1,17 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:mall_app/constants/app_theme.dart';
+import 'package:flutter_application_1/constants/app_theme.dart';
+import 'package:flutter_application_1/routes.dart';
 
 class CustomDialogContestTips extends StatefulWidget {
-
-
-  const CustomDialogContestTips(
-      {Key? key,
-    })
-      : super(key: key);
+  const CustomDialogContestTips({
+    Key? key,
+  }) : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
-  _CustomDialogContestTipsState createState() => _CustomDialogContestTipsState();
+  _CustomDialogContestTipsState createState() =>
+      _CustomDialogContestTipsState();
 }
 
 class _CustomDialogContestTipsState extends State<CustomDialogContestTips> {
@@ -32,29 +33,208 @@ class _CustomDialogContestTipsState extends State<CustomDialogContestTips> {
 
   contentBox(context) {
     return Container(
-      padding: const EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 10),
-      margin: const EdgeInsets.only(top: 40),
+      padding: const EdgeInsets.only(left: 15, top: 10, right: 15, bottom: 10),
+      margin: const EdgeInsets.symmetric(vertical: 100),
       decoration: BoxDecoration(
         color: AppColors.scaffoldBackGroundColor,
         borderRadius: BorderRadius.circular(10),
       ),
       child: Padding(
-        padding: const EdgeInsets.only(top: 40),
+        padding: const EdgeInsets.only(top: 10),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: Text(
+                'خطوات بدأ المسابقة',
+                style: const TextStyle(
+                    fontSize: 20.0,
+                    color: AppColors.blackLightColor,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '1- ',
+                        style: const TextStyle(
+                            fontSize: 15.0, color: AppColors.blackLightColor),
+                        textAlign: TextAlign.start,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'يجب أن تكون انت ضمن أحد المولات أو المحلات التجارية المشاركة بالتطبيق.',
+                          style: const TextStyle(
+                              fontSize: 15.0, color: AppColors.blackLightColor),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '2- ',
+                        style: const TextStyle(
+                            fontSize: 15.0, color: AppColors.blackLightColor),
+                        textAlign: TextAlign.start,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'اضغط على بدء المسابقة.',
+                          style: const TextStyle(
+                              fontSize: 15.0, color: AppColors.blackLightColor),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '3- ',
+                        style: const TextStyle(
+                            fontSize: 15.0, color: AppColors.blackLightColor),
+                        textAlign: TextAlign.start,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'اختر من القائمة المول أو المحل المتواجد به حاليا.',
+                          style: const TextStyle(
+                              fontSize: 15.0, color: AppColors.blackLightColor),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '4- ',
+                        style: const TextStyle(
+                            fontSize: 15.0, color: AppColors.blackLightColor),
+                        textAlign: TextAlign.start,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'سجل رقم فاتورة الشراء الخاصة بك التابعة لأحد المحلات المشتركة في التطبيق للبدء في المسابقة.',
+                          style: const TextStyle(
+                              fontSize: 15.0, color: AppColors.blackLightColor),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '5- ',
+                        style: const TextStyle(
+                            fontSize: 15.0, color: AppColors.blackLightColor),
+                        textAlign: TextAlign.start,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'مسح الباركود للمحلات وجمع النقاط للفوز بالجوائز اليومية.',
+                          style: const TextStyle(
+                              fontSize: 15.0, color: AppColors.blackLightColor),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '6- ',
+                        style: const TextStyle(
+                            fontSize: 15.0, color: AppColors.blackLightColor),
+                        textAlign: TextAlign.start,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'عند جمع رصيد محدد من النقاط(500 نقطة) يؤهلك ذلك للانتقال للمراحل التالية.',
+                          style: const TextStyle(
+                              fontSize: 15.0, color: AppColors.blackLightColor),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Platform.isIOS
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                'إن المسابقات والجوائز المقدمة ليست من تنظيم أو تقديم أو دعم شركة Aplle',
+                                style: const TextStyle(
+                                    fontSize: 15.0, color: AppColors.redColor),
+                                textAlign: TextAlign.start,
+                              ),
+                            ),
+                          ],
+                        )
+                      : Container(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'يمكنكم قراءة المزيد ',
+                        style: const TextStyle(
+                            fontSize: 14.0, color: AppColors.blackLightColor),
+                        textAlign: TextAlign.start,
+                      ),
+                      InkWell(
+                        child: Text(
+                          'حول المسابقات.',
+                          style: const TextStyle(
+                              fontSize: 14.0, color: Colors.blue),
+                          textAlign: TextAlign.start,
+                        ),
+                        onTap: () {
+                          Navigator.of(context).pop();
+                          Navigator.pushNamed(context, Routes.aboutScreen);
+                        },
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            /*
             Text(
               'ادخل على المول المتواجد به أو ضمن المحلات التجارية وسجل رقم فاتورة الشراء الخاصة بك وذلك عن طريق أحد المحلات المشتركة في التطبيق للبدء في المسابقة من ثم مسح الباركود للمحلات وجمع النقاط للفوز بالجوائز اليومية ليؤهلك ذلك بالدخول إلى لمسابقات الأسبوعية والشهرية والثلاث أشهر في حال شرائك أي منتجات أخرى من المحلات المشتركة في تطبيق بريق الإعلان (معنا الكل فائز).',
               style: const TextStyle(
-                  fontSize: 20.0, color: AppColors.blackLightColor),
+                  fontSize: 16.0, color: AppColors.blackLightColor),
               textAlign: TextAlign.center,
             ),
+            */
             const SizedBox(
               height: 15,
             ),
-
             Align(
               alignment: Alignment.bottomCenter,
               child: TextButton(
